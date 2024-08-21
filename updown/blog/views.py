5d78d41dafb6e0561eb83f103cb527db3e58dd31
David Portilla <94.david.pm@gmail.com>
2024-08-21 15:13:18 -0600

    User registration modules

--- updown/blog/views.py
+++ updown/blog/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-	{
-		'author': 'User1',
-		'title': 'Post 2',
-		'content': 'Post 2',
-		'date_posted': '13-04-13'
-	},
-	{
-		'author': 'User2',
-		'title': 'Post 1',
-		'content': 'Post 1',
-		'date_posted': '14-04-14'
-	}
-]
-
-
 def home(request):
     context = {
 		'posts': Post.objects.all()
