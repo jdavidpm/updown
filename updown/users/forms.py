ef2528f089f14565b62314ea4c99771b9de614b3
David Portilla <94.david.pm@gmail.com>
2024-08-22 11:42:53 -0600

    User profile and picture

--- updown/users/forms.py
+++ updown/users/forms.py
@@ -3,9 +3,15 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-        
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'user@server.com', 'autocomplete': 'off'}))
     class meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'usable_password']
-    field_order = ['username', 'email', 'password1', 'password2', 'usable_password']
+        fields = ('username', 'email')
+    field_order = ('username', 'email')
+    
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
--- updown/users/views.py
+++ updown/users/views.py
@@ -8,8 +8,7 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account was created')
+            messages.success(request, f'Your account was created.')
             return redirect('login')
     else:
         form = UserRegisterForm()
