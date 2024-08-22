12b11ff3cc2b000928e47052f47429ccdad50d89
David Portilla <94.david.pm@gmail.com>
2024-08-22 13:17:54 -0600

    Update user profile

--- updown/users/forms.py
+++ updown/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'user@server.com', 'autocomplete': 'off'}))
@@ -15,3 +16,23 @@ class UserRegisterForm(UserCreationForm):
         if commit:
             user.save()
         return user
+    
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+    
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('image',)
+    
+    def save(self, commit=True):
+        profile = super(ProfileUpdateForm, self).save(commit=False)
+        profile.image = self.cleaned_data["image"]
+        if commit:
+            profile.save()
+        return profile
